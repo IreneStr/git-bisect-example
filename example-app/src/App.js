5e127dd6c9d3ec3daf3928c2ac07cf023301e138
IreneStr <irene@yoast.com>
2019-07-02 15:35:19 +0200

    Add paragraph about the button

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
 			<Paragraph text={ "Git bisect can come in very handy." }/>
 			<Paragraph text={ "How many steps will it take to find the faulty commit?" }/>
 			<Button>Click me!</Button>
-
+			<Paragraph text={ "That's a fake button ^" }/>
 		</Fragment>
 
 	);
