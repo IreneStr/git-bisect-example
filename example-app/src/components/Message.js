7c0d1010270bef3647f0cd8ffac711fd057f7e76
IreneStr <irene@yoast.com>
2019-07-02 09:58:19 +0200

    Increase title font size and add exclamation mark

--- example-app/src/components/Message.js
+++ example-app/src/components/Message.js
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 
 const StyledMessage = styled.h1`
 	color: purple;
+	font-size: 72px;
 `;
 
 class Message extends React.Component {
 	render() {
-		return <StyledMessage>Hello, { this.props.name }</StyledMessage>;
+		return <StyledMessage>Hello, { this.props.name }!</StyledMessage>;
 	}
 }
 export default Message;
